48390f0f5e0c912328def0954cbb197554822c95
codiren <82444600+codiren@users.noreply.github.com>
2025-04-01 13:07:44 +0300

    Bugfix getAIScore does not work when text has new line characters

--- be/server.php
+++ be/server.php
@@ -78,7 +78,8 @@ function getAiAnalysis($userCode, $maxAttempts = 5) {
 function getAIScore($userCode) {
 	$command = "echo \"Respond only(user will only see the number between those tags, so dont give any explanation so your response would run faster) with code quality(0-10), acceptable answer: <codeScore>0</codeScore> now I'm giving you user message: $userCode\" | ollama run deepseek-r1";
 	echo $command;
-    $output = shell_exec($command);
+	$escapedPrompt = prepareStringForCmd($command);
+    $output = shell_exec($escapedPrompt);
     echo $output;
     preg_match('/<codeScore>(.|\n)*?<\/codeScore>/', $output, $matches);
     return $matches[1] ?? 0;
